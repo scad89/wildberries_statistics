b0b7d0ad85703b6965412fd695430d88704ee9f4
scad89 <scad200@gmail.com>
2022-05-12 00:27:13 +0300

    Add serializers, urls and views for articles

--- wildberries_robot/cards/models.py
+++ wildberries_robot/cards/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
 
-class UserArticul(models.Model):
-    articul = models.PositiveBigIntegerField()
-    unvisible = models.BooleanField(default=False)
+class UserArticle(models.Model):
+    article = models.PositiveBigIntegerField()
+    unvisible = models.BooleanField(
+        verbose_name='Do you want to delete an article?', default=False)
+
+    def __str__(self):
+        return self.articul
+
+    class Meta:
+        verbose_name = 'User Article'
+        verbose_name_plural = 'user Articles'
 
 
 class RecordCard(models.Model):
-    id_articul = models.ForeignKey(
-        UserArticul, on_delete=models.CASCADE, related_name='user_articul')
+    id_article = models.ForeignKey(
+        UserArticle, on_delete=models.CASCADE, related_name='user_article')
     name_of_product = models.CharField(max_length=255)
     price_without_discount = models.PositiveIntegerField()
     price_with_discount = models.PositiveIntegerField()
     brand = models.CharField(max_length=255)
     supplier = models.CharField(max_length=255)
     record_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id_articul,}, {self.name_of_product}'
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
